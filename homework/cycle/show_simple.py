38df9abed38f473ba5d1e808487d271c42cce6d0
nikita <nbarabanschikov@mts.ru>
2023-10-26 22:49:10 +0300

    add comments

--- homework/cycle/show_simple.py
+++ homework/cycle/show_simple.py
@@ -10,24 +10,25 @@
 
 # P.S. Код также должен легко модифицироваться для любых других интервалов.
 
-# def add(x, y):
-#     return x + y
 
 
 import math # импортирую библиотеку что бы использовать мат функции
 # math.sqrt вернуть корень числа
 
+# для проверки числа на простоту  мы делим число на ряд чисел до корня исследумое числа
+# пример корень из 9 равен 3
+# для проверки 9 на простоту мы должны разделить 9 на число 2, а потом на число 3, и проверить наличие нулевого остатка
+
 stard = 2 # определяю начало диапозона
 finishd = 10 # определяю конец диапозона
-currentnumber = stard
+currentnumber = stard # определяю что делимое из цикла будет равно началу диапозона
+delitel = stard # определяю что делитель из цикла будет равно началу диапозона
 
-delitel = stard
-
-while currentnumber >= stard and currentnumber <= finishd:
+while currentnumber >= stard and currentnumber <= finishd: # делимое должно быть равно началу диапозона и не быть больше его конца
     currentnumber = currentnumber + 1
-    while delitel >= stard and delitel <= math.sqrt(currentnumber):
+    while delitel >= stard and delitel <= math.sqrt(currentnumber): # делитель должен быть равен началу диапозон и не превашать корень делимого
         delitel = delitel + 1
-    if currentnumber % delitel == 0:
+    if currentnumber % delitel == 0: # если результат деления имеет нулевой остаток, значит число не является простым, и выаодить мы его не будем
             continue
             
     print(currentnumber)
