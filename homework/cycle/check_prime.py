16e06a02f6abce640da91e533bef205a33f2a316
nikita <nbarabanschikov@mts.ru>
2023-12-07 09:41:00 +0300

    add

--- homework/cycle/check_prime.py
+++ homework/cycle/check_prime.py
@@ -1,5 +1,14 @@
 number = 13
-collection = [2,3,4,5,6,7,8,9,10,11,12]
+# collection = [2,3,4,5,6,7,8,9,10,11,12]
+pupa = range (2,number-1)
 
-for i in collection:
-    if number % i ==
+for i in pupa:
+    if number % i != 0:
+        
+        i +=1
+
+    else: print ("Число" , number , "являeтся составным")
+print ("Число" , number , "являeтся простым")
+
+
+# подправить для составных чисел
