bc22a2fec3b98d4940e18475731b44b5c452cb92
nikita <nbarabanschikov@mts.ru>
2023-12-12 20:34:47 +0300

    add python

--- homework/cycle/new_check_prime.py
+++ homework/cycle/new_check_prime.py
@@ -1,8 +1,21 @@
+# number = int(input())
+# prime = True
+# for i in range(2,  int(number ** 0.5) + 1):
+#     if number % i == 0:
+#         prime == False
+#         print("Число", number, "cоставное")
+#         break
+#     if prime == True:
+#         print("Число", number, "простое")
+
+
 number = int(input())
 prime = True
-for i in range(2,  int(x**0, 5) + 1):
+for i in range(2,  number):
     if number % i == 0:
-        prime == False
-        print("Число", number, "cоставное")
+        prime = False
+        break
 if prime == True:
     print("Число", number, "простое")
+else:
+    print("Число", number, "cоставное")
