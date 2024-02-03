ed6f59a332a0cea926841782540f8056afee71f6
nikita <nbarabanschikov@mts.ru>
2024-02-03 12:54:46 +0300

    add function

---
+++ homework/02.2024_homework/call_function.py
@@ -0,0 +1,4 @@
+start_number = int(input())
+finish_number = int(input())
+
+from check_def import check_prime
--- homework/02.2024_homework/check_def.py
+++ homework/02.2024_homework/check_def.py
@@ -1,18 +1,19 @@
-start_number = int(input())
-finish_number = int(input())
+# start_number = int(input())
+# finish_number = int(input())
 
 
-for number_for_check in range (start_number, finish_number +1):
-    prime = True
-    for deliter in range (2, number_for_check):
-        if number_for_check % deliter == 0:
-            prime = False
-            print("Число {} составное".format(number_for_check))
-            break
-    if prime == True:
-        print("Число {} простое".format(number_for_check))
+def check_prime (start_number, finish_number):
+    for number_for_check in range (start_number, finish_number +1):
+        prime = True
+        for deliter in range (2, number_for_check):
+            if number_for_check % deliter == 0:
+                prime = False
+                print("Число {} составное".format(number_for_check))
+                break
+        if prime == True:
+            print("Число {} простое".format(number_for_check))
         
     
     
     
-
+check_prime(3, 10)
