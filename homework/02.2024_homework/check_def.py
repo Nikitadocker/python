11305d3185711dc169e70eab5acb2fb200854fbe
nikita <nbarabanschikov@mts.ru>
2024-02-03 09:21:14 +0300

    add

---
+++ homework/02.2024_homework/check_def.py
@@ -0,0 +1,18 @@
+start_number = int(input())
+finish_number = int(input())
+
+
+for number_for_check in range (start_number, finish_number +1):
+    prime = True
+    for deliter in range (2, number_for_check):
+        if number_for_check % deliter == 0:
+            prime = False
+            print("Число {} составное".format(number_for_check))
+            break
+    if prime == True:
+        print("Число {} простое".format(number_for_check))
+        
+    
+    
+    
+
