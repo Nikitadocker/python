875edffbfded5e7195b5ba37eecabb8c9a796c13
nikita <nbarabanschikov@mts.ru>
2024-02-04 20:51:24 +0300

    add homework

--- homework/02.2024_homework/check_input_module.py
+++ homework/02.2024_homework/check_input_module.py
@@ -1,15 +1,15 @@
 def check_input(start_number, finish_number):
-        while start_number < 2 or finish_number < 2:
-            print("Простые числа не могу быть меньше 2 Повторите ввод")
-            start_number = int(input())
-            finish_number = int(input())
-        while start_number > finish_number:
-            print("Конец диапозона должен быть больше чем начало диапозона Повторите ввод")
-            start_number = int(input())
-            finish_number = int(input())
-        while start_number == finish_number:
-            print("Начало диапозона и конец диапозона совпадают Повторите ввод")
-            start_number = int(input())
-            finish_number = int(input())
-   
+    if start_number < 2 or finish_number < 2:
+        print(' Числа меньше 2 не могут быть простыми. Повторите ввод ')
+        
+        return False
+    elif start_number == finish_number:
+        print(' Введенные значение совпадают. Повторите ввод ')
+        return False
+    elif start_number > finish_number:
+        print('Начало диапозона не должно быть чем конец диапзона Повторите ввод')
+        return False
+    else: 
+        return True
+
 
