2335f7d8d8e00d5c8bc63691649f237e18376a6d
nikita <nbarabanschikov@mts.ru>
2024-02-05 15:11:36 +0300

    add homework

---
+++ homework/04.02.2024_homework/check_input_module.py
@@ -0,0 +1,15 @@
+def check_input(start_number, finish_number):
+    if start_number < 2 or finish_number < 2:
+        print(' Числа меньше 2 не могут быть простыми. Повторите ввод ')
+        
+        return False
+    elif start_number >= finish_number:  
+        print(' Начало диапозона не должно быть больше или равно концу диапозона. Повторите ввод ')
+        return False
+    # elif start_number > finish_number:
+    #     print('Начало диапозона не должно быть чем конец диапзона Повторите ввод')
+    #     return False
+    else: 
+        return True
+
+
---
+++ homework/04.02.2024_homework/check_prime_module.py
@@ -0,0 +1,15 @@
+def check_prime (start_number, finish_number):
+    for number_for_check in range (start_number, finish_number +1):
+        prime = True
+        for deliter in range (2, number_for_check):
+            if number_for_check % deliter == 0:
+                prime = False
+                print("Число {} составное".format(number_for_check))
+                break
+        if prime == True:
+            print("Число {} простое".format(number_for_check))
+        
+    
+    
+    
+
