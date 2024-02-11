2199aeb1a1b33b5ce9b09490b33e3c62a5e140e5
nikita <nbarabanschikov@mts.ru>
2024-02-11 18:30:34 +0300

    add

---
+++ homework/09.02.2024_homework/test.py
@@ -0,0 +1,33 @@
+import subprocess
+import json
+import math
+
+result = subprocess.run(['ps', 'aux', '--no-headers'], capture_output=True, text=True)
+
+pids = []
+
+for line in result.stdout.splitlines():
+    # Разделить строку по пробелам
+    fields = line.split()
+    # Извлечь PID из второго столбца
+    pid = fields[1]
+    # Добавить PID в список
+    pids.append(int(pid))
+
+result = {}
+
+for pid_for_check in pids:
+    result[pid_for_check]='Prime'
+    deviders = range(2, int(math.sqrt(pid_for_check) + 1))                             
+    for current_devider in deviders:
+        if pid_for_check % current_devider == 0:
+            result[pid_for_check]='Composite'
+            break
+        else:
+            result[pid_for_check]='Prime'
+
+
+with open ('output.json', 'w') as file:
+    json.dump(result, file)
+    
+    
