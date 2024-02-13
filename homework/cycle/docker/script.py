bd852ab864462d31c8a83d328a0bbf5b6dd949c3
nikita <nbarabanschikov@mts.ru>
2024-02-13 18:49:37 +0300

    del

--- homework/cycle/docker/script.py
+++
@@ -1,9 +0,0 @@
-import time
-import os
-
-# Чтение значения переменной окружения
-app_version = os.environ.get('APP_VERSION', 'unknown')
-
-while True:
-    print(f'Hi! I have version {app_version}')
-    time.sleep(10)
