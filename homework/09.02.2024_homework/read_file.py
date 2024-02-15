488f449e0f9177a64118e67308059654e69bd1e2
nikita <nbarabanschikov@mts.ru>
2024-02-15 17:05:24 +0300

    fix read file

--- homework/09.02.2024_homework/read_file.py
+++ homework/09.02.2024_homework/read_file.py
@@ -1,2 +1,31 @@
 import json
 from dictionary import check_prime
+
+with open ('output_pids.json') as file_read:
+
+    file_check = json.load(file_read)  # <class 'list'>
+    
+    # print(type(file_check))
+
+
+for key in file_check:
+    prime_or_composite = key["Prime or Composite"]
+    
+    pid_for_check = key["pid_number"]
+    
+    result_after_check = check_prime (int(pid_for_check))
+    
+    if result_after_check == "Prime":
+        prime_or_composite == "Prime"
+    elif result_after_check == "Composite":
+            prime_or_composite == "Composite"
+    elif prime_or_composite == "Not Prime and Composite":
+        print("Цифра 1 является уникальной. Это не ошибка")
+        
+    else:
+        print("Найдена ошибка. Число должно быть Простым")
+    
+    # print(pid_for_check)
+    # print(prime_or_composite)
+    # print (result_after_check)
+    
