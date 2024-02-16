4c6988cd0e09dbb667cafe696d6888126f64c1bf
nikita <nbarabanschikov@mts.ru>
2024-02-16 14:13:01 +0300

    commit

--- homework/09.02.2024_homework/read_file.py
+++ homework/09.02.2024_homework/read_file.py
@@ -15,15 +15,14 @@ for key in file_check:
     
     result_after_check = check_prime (int(pid_for_check))
     
-    if result_after_check == "Prime" and prime_or_composite == "Prime":
-        print("Простые числа проверены. Все хорошо")
-    elif result_after_check == "Composite" and prime_or_composite == "Composite":
-            print("Cоставные числа проверены. Все хорошо")
+    if result_after_check == "Prime" and prime_or_composite != "Prime":
+        print("Число, {} проверено. Найдена ошибка. Число должно быть простым".format(pid_for_check))
+    elif result_after_check == "Composite" and prime_or_composite != "Composite":
+        print("Число, {} проверено. Найдена ошибка. Число должно быть составным".format(pid_for_check))
     elif prime_or_composite == "Not Prime and Composite":
         print("Цифра 1 является уникальной. Это не ошибка")
         
-    else:
-        print("Найдена ошибка. Число должно быть Простым")
+    
     
    
     
